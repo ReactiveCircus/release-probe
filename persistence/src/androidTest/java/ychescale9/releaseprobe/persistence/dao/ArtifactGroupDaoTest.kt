94aad8273d8b83dd858a715181813bb80ef685f7
Yang Chen <ychescale9@gmail.com>
2018-08-07 21:06:35 +1000

    Update AndroidX to rc01, build tools to 28.0.2, and docker image.

--- persistence/src/androidTest/java/ychescale9/releaseprobe/persistence/dao/ArtifactGroupDaoTest.kt
+++ persistence/src/androidTest/java/ychescale9/releaseprobe/persistence/dao/ArtifactGroupDaoTest.kt
@@ -1,11 +1,7 @@
 package ychescale9.releaseprobe.persistence.dao
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.room.Room
-import androidx.test.InstrumentationRegistry
 import androidx.test.filters.MediumTest
-import org.junit.After
-import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
