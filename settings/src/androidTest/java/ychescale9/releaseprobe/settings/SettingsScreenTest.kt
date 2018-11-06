9c5fec20252c2ecc2072e11493d6ed5dcc27c5e9
Yang Chen <ychescale9@gmail.com>
2018-11-07 00:29:35 +1100

    Use the new FragmentScenario to drive fragment UI tests.

--- settings/src/androidTest/java/ychescale9/releaseprobe/settings/SettingsScreenTest.kt
+++ settings/src/androidTest/java/ychescale9/releaseprobe/settings/SettingsScreenTest.kt
@@ -1,22 +1,15 @@
 package ychescale9.releaseprobe.settings
 
 import androidx.test.filters.LargeTest
-import androidx.test.rule.ActivityTestRule
-import org.junit.Rule
 import org.junit.Test
 import ychescale9.releaseprobe.testing.BaseScreenTest
-import ychescale9.releaseprobe.testing.SingleFragmentActivity
 
 @LargeTest
 class SettingsScreenTest : BaseScreenTest() {
 
-    @Rule
-    @JvmField
-    val activityRule = ActivityTestRule(SingleFragmentActivity::class.java, true, false)
-
     @Test
     fun openSettingsScreenWithDefaultSettings_defaultSettingsDisplayed() {
-        launchActivityWithFragment(activityRule, SettingsFragment())
+        launchFragment<SettingsFragment>()
         // TODO
     }
 }
