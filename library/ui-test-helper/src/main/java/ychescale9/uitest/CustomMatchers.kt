8d08d24b948f6cb800532f7a52bae13b2052364f
Yang Chen <ychescale9@gmail.com>
2019-01-29 22:21:07 +1100

    Upgrade AGP, Kotlin, and library dependencies. Fix UI tests. Revert navigation to alpha08 as safeargs plugin does not work with new AGP.

--- library/ui-test-helper/src/main/java/ychescale9/uitest/CustomMatchers.kt
+++ library/ui-test-helper/src/main/java/ychescale9/uitest/CustomMatchers.kt
@@ -149,10 +149,10 @@ fun withToolbarNavigationButton(): Matcher<View> {
             )))
 }
 
-fun withToolbarTitle(textMatcher: Matcher<CharSequence>): Matcher<Any> {
+fun withToolbarTitle(textMatcher: Matcher<String>): Matcher<Any> {
     return object : BoundedMatcher<Any, Toolbar>(Toolbar::class.java) {
         public override fun matchesSafely(toolbar: Toolbar): Boolean {
-            return textMatcher.matches(toolbar.title)
+            return textMatcher.matches(toolbar.title.toString())
         }
 
         override fun describeTo(description: Description) {
