fa82975094821de2a03114120e35ace6ded7ed77
Yang Chen <ychescale9@gmail.com>
2019-03-14 19:42:08 +1100

    Fix blocking execution for UseCase, update tests to verify blocking vs non-blocking execution properly.

--- library/core-infra/src/test/java/ychescale9/infra/domain/UseCaseTest.kt
+++ library/core-infra/src/test/java/ychescale9/infra/domain/UseCaseTest.kt
@@ -1,8 +1,10 @@
 package ychescale9.infra.domain
 
 import io.reactivex.Observable
+import io.reactivex.Scheduler
 import io.reactivex.observers.TestObserver
 import io.reactivex.schedulers.Schedulers
+import io.reactivex.schedulers.TestScheduler
 import org.amshove.kluent.shouldBe
 import org.junit.Test
 import ychescale9.infra.SchedulerProvider
@@ -14,14 +16,34 @@ class UseCaseTest {
 
     lateinit var testObserver: TestObserver<String>
 
-    private val useCase = UseCaseImpl(TestSchedulerProvider())
-    private val emptyParamsUseCase = EmptyParamsUseCaseImpl(TestSchedulerProvider())
+    private val ioScheduler = TestScheduler()
+    private val uiScheduler = TestScheduler()
+
+    private val schedulerProvider = object : SchedulerProvider {
+        override fun io(): Scheduler {
+            return ioScheduler
+        }
+
+        override fun ui(): Scheduler {
+            return uiScheduler
+        }
+
+        override fun computation(): Scheduler {
+            return Schedulers.trampoline()
+        }
+    }
+
+    private val useCase = UseCaseImpl(schedulerProvider)
+    private val emptyParamsUseCase = EmptyParamsUseCaseImpl(schedulerProvider)
 
     @Test
     fun `should receive value when subscribed with params`() {
         testObserver = useCase.buildObservable(UseCaseImpl.Params(true)).test()
-        testObserver.awaitTerminalEvent()
 
+        ioScheduler.triggerActions()
+        uiScheduler.triggerActions()
+
+        testObserver.awaitTerminalEvent()
         testObserver.assertValue(dummyResult1)
         testObserver.assertNoErrors()
     }
@@ -29,8 +51,11 @@ class UseCaseTest {
     @Test
     fun `should receive value when subscribed with empty params`() {
         testObserver = emptyParamsUseCase.buildObservable(EmptyParams()).test()
-        testObserver.awaitTerminalEvent()
 
+        ioScheduler.triggerActions()
+        uiScheduler.triggerActions()
+
+        testObserver.awaitTerminalEvent()
         testObserver.assertValue(dummyResult1)
         testObserver.assertNoErrors()
     }
@@ -38,8 +63,8 @@ class UseCaseTest {
     @Test
     fun `should receive value synchronously when subscribed with params`() {
         testObserver = useCase.buildObservable(UseCaseImpl.Params(true), blocking = true).test()
-        testObserver.awaitTerminalEvent()
 
+        testObserver.awaitTerminalEvent()
         testObserver.assertValue(dummyResult1)
         testObserver.assertNoErrors()
     }
@@ -47,8 +72,8 @@ class UseCaseTest {
     @Test
     fun `should receive value synchronously when subscribed with empty params`() {
         testObserver = emptyParamsUseCase.buildObservable(EmptyParams(), blocking = true).test()
-        testObserver.awaitTerminalEvent()
 
+        testObserver.awaitTerminalEvent()
         testObserver.assertValue(dummyResult1)
         testObserver.assertNoErrors()
     }
@@ -81,13 +106,4 @@ private class EmptyParamsUseCaseImpl(
     override fun createUseCase(): Observable<String> {
         return Observable.just(dummyResult1)
     }
-}
-
-private class TestSchedulerProvider : SchedulerProvider {
-
-    override fun ui() = Schedulers.trampoline()
-
-    override fun io() = Schedulers.trampoline()
-
-    override fun computation() = Schedulers.trampoline()
 }
