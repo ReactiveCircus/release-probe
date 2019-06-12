c3620f55fb8ba1b2a34a6fba9a00a616d0817e1a
Yang Chen <ychescale9@gmail.com>
2019-06-13 00:09:41 +1000

    LeakCanary 2.0 alpha2.

--- app/src/debug/java/ychescale9/releaseprobe/DebugReleaseProbeApp.kt
+++ app/src/debug/java/ychescale9/releaseprobe/DebugReleaseProbeApp.kt
@@ -1,7 +1,6 @@
 package ychescale9.releaseprobe
 
 import android.os.StrictMode
-import com.squareup.leakcanary.LeakCanary
 import timber.log.Timber
 
 class DebugReleaseProbeApp : ReleaseProbeApp() {
@@ -11,12 +10,6 @@ class DebugReleaseProbeApp : ReleaseProbeApp() {
 
         // turn on strict mode for Debug build
         StrictMode.enableDefaults()
-    }
-
-    override fun installLeakCanary() {
-        LeakCanary.refWatcher(this)
-                .listenerServiceClass(LeakUploadService::class.java)
-                .buildAndInstall()
     }
 
     override fun initializeTimber() {
