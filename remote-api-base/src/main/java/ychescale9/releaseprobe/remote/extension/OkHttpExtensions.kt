0e44b04321039923022d9c2db98d9041a187abac
Yang Chen <ychescale9@gmail.com>
2019-06-27 19:47:50 +1000

    Upgrade to OkHttp 4.0.0, fix deprecated APIs.

--- remote-api-base/src/main/java/ychescale9/releaseprobe/remote/extension/OkHttpExtensions.kt
+++
@@ -1,9 +0,0 @@
-package ychescale9.releaseprobe.remote.extension
-
-import okhttp3.OkHttpClient
-
-inline fun OkHttpClient.Builder.build(builder: OkHttpClient.Builder.() -> Unit): OkHttpClient {
-    return OkHttpClient.Builder().apply {
-        builder(this)
-    }.build()
-}
--- remote-api-base/src/main/java/ychescale9/releaseprobe/remote/extension/RetrofitExtensions.kt
+++
@@ -1,9 +0,0 @@
-package ychescale9.releaseprobe.remote.extension
-
-import retrofit2.Retrofit
-
-inline fun Retrofit.Builder.build(builder: Retrofit.Builder.() -> Unit): Retrofit {
-    return Retrofit.Builder().apply {
-        builder(this)
-    }.build()
-}
