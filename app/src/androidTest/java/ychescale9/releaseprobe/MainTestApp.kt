d49d02945bf826c1c78a8f9b427f77f6f4641bf8
Yang Chen <ychescale9@gmail.com>
2019-07-05 18:29:22 +1000

    LeakCanary 2.0-alpha-3. Disable LeakCanary heap dump in base test app.

--- app/src/androidTest/java/ychescale9/releaseprobe/MainTestApp.kt
+++ app/src/androidTest/java/ychescale9/releaseprobe/MainTestApp.kt
@@ -1,17 +1,11 @@
 package ychescale9.releaseprobe
 
-import leakcanary.LeakCanary
 import org.koin.core.module.Module
 import org.koin.dsl.module
 import ychescale9.releaseprobe.testing.ScreenTestApp
 import ychescale9.releaseprobe.testing.di.testModules
 
 class MainTestApp : ScreenTestApp() {
-
-    override fun onCreate() {
-        super.onCreate()
-        LeakCanary.config = LeakCanary.config.copy(dumpHeap = false)
-    }
 
     private val mainModule = module {
         // TODO
