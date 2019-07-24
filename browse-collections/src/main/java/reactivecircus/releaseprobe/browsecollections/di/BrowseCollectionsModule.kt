72431dbbbf2c301822895bfaf099c3ffeb8789ca
Yang Chen <ychescale9@gmail.com>
2019-07-24 17:47:00 +1000

    Add Blueprint libraries 1.0.0-SNAPSHOT, remove local Blueprint sources.

--- browse-collections/src/main/java/reactivecircus/releaseprobe/browsecollections/di/BrowseCollectionsModule.kt
+++ browse-collections/src/main/java/reactivecircus/releaseprobe/browsecollections/di/BrowseCollectionsModule.kt
@@ -1,9 +1,11 @@
 package reactivecircus.releaseprobe.browsecollections.di
 
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 import reactivecircus.releaseprobe.browsecollections.ArtifactCollectionsViewModel
 
+@ExperimentalCoroutinesApi
 val browseCollectionsModule = module {
 
     viewModel {
