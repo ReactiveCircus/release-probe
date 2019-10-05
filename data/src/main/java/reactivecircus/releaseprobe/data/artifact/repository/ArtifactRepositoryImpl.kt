c02e55888103d8baf2ffce363767b12d0b258335
Yang Chen <ychescale9@gmail.com>
2019-10-05 16:52:58 +1000

    Coroutines 1.3.2, MDC 1.1.0-beta01, OkHttp 4.2.1, RxJava 2.2.13.

--- data/src/main/java/reactivecircus/releaseprobe/data/artifact/repository/ArtifactRepositoryImpl.kt
+++ data/src/main/java/reactivecircus/releaseprobe/data/artifact/repository/ArtifactRepositoryImpl.kt
@@ -3,7 +3,6 @@ package reactivecircus.releaseprobe.data.artifact.repository
 import com.nytimes.android.external.store3.base.impl.BarCode
 import com.nytimes.android.external.store3.base.impl.room.StoreRoom
 import io.reactivex.BackpressureStrategy
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.reactive.asFlow
 import reactivecircus.releaseprobe.domain.artifact.model.ArtifactGroup
@@ -13,14 +12,12 @@ class ArtifactRepositoryImpl(
     private val artifactGroupsWithArtifactsStore: StoreRoom<List<ArtifactGroup>, BarCode>
 ) : ArtifactRepository {
 
-    @ExperimentalCoroutinesApi
     override fun streamAllArtifactGroups(): Flow<List<ArtifactGroup>> {
         return artifactGroupsWithArtifactsStore.get(BarCode.empty())
             .toFlowable(BackpressureStrategy.LATEST)
             .asFlow()
     }
 
-    @ExperimentalCoroutinesApi
     override fun fetchAllArtifactGroups(): Flow<List<ArtifactGroup>> {
         return artifactGroupsWithArtifactsStore.fetch(BarCode.empty())
             .toFlowable(BackpressureStrategy.LATEST)
