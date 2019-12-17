c98294d942952cbee9ceb3a462bb9b5fe09b7869
Yang Chen <ychescale9@gmail.com>
2019-12-17 22:11:59 +1100

    Simplify ViewModels with Flow -> LiveData extension.

--- ui-browse-collections/src/main/java/reactivecircus/releaseprobe/browsecollections/BrowseCollectionsViewModel.kt
+++ ui-browse-collections/src/main/java/reactivecircus/releaseprobe/browsecollections/BrowseCollectionsViewModel.kt
@@ -1,15 +1,11 @@
 package reactivecircus.releaseprobe.browsecollections
 
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.asLiveData
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 import reactivecircus.blueprint.interactor.EmptyParams
 import reactivecircus.releaseprobe.domain.artifactcollection.interactor.StreamArtifactCollections
-import reactivecircus.releaseprobe.domain.artifactcollection.model.ArtifactCollection
 import timber.log.Timber
 
 @UseExperimental(ExperimentalCoroutinesApi::class)
@@ -17,16 +13,7 @@ class BrowseCollectionsViewModel(
     streamArtifactCollections: StreamArtifactCollections
 ) : ViewModel() {
 
-    val artifactCollectionsLiveData = MutableLiveData<List<ArtifactCollection>>()
-
-    init {
-        streamArtifactCollections.buildFlow(EmptyParams)
-            .onEach {
-                artifactCollectionsLiveData.value = it
-            }
-            .catch {
-                Timber.e(it, "State machine stream terminated.")
-            }
-            .launchIn(viewModelScope)
-    }
+    val artifactCollectionsLiveData = streamArtifactCollections.buildFlow(EmptyParams)
+        .catch { Timber.e(it, "State machine stream terminated.") }
+        .asLiveData()
 }
