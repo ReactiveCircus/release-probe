54b1020965d746e40f6e10a69d97acd9e582587c
Yang Chen <ychescale9@gmail.com>
2020-02-15 16:35:46 +1100

    Reference specific library resource from app module.

--- app/src/main/java/reactivecircus/releaseprobe/MainActivity.kt
+++ app/src/main/java/reactivecircus/releaseprobe/MainActivity.kt
@@ -2,13 +2,14 @@ package reactivecircus.releaseprobe
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import reactivecircus.releaseprobe.core.R as ResourcesR
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setTheme(R.style.Theme_ReleaseProbe_DayNight)
+        setTheme(ResourcesR.style.Theme_ReleaseProbe_DayNight)
 
         setContentView(R.layout.activity_main)
     }
